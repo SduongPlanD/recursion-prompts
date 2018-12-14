b620d8cd7ba1c30622d1b0856dadefdaeabc96f0
Steve <steve@Plan.D>
2018-12-13 16:42:10 -0800

    extra recursion problems 11 - 14.5

---
+++ src/new-pair/recursion-new-pair.js
@@ -0,0 +1,317 @@
+
+// 11. Write a function that returns the remainder of x divided by y without using the
+// modulo (%) operator. 
+// modulo(5,2) // 1
+// modulo(17,5) // 2
+// modulo(22,6) // 4
+ var modulo = function(x, y) { // (2,1) -> (1,1)
+ 	// console.log('x',x,'y', y);
+ 	if(x === 0 && y ===0){
+		return NaN;
+	}
+
+	if(x >= 0 && y > 0){ //If x and y are positive or x and y are negative
+		//see if x is greater than y
+		if (x >= y){ 
+			// if it is, modulo( x-y)
+			return modulo(x-y, y); // 1] (1,1)
+		}else{
+			// if it isn't , return x
+			return x;
+		};
+	};
+
+	if(x <= 0 && y < 0){ //If x and y are both negative
+		//see if x is greater than y
+		if (x <= y){
+			// if it is, modulo( x-y)
+			return modulo(x - y, y); // 1] (1,1) 
+		}else{
+			// if it isn't , return x
+			return x;
+		}
+	}
+
+	if(x <= 0 || y < 0){ //Either x and y are negative (4, 2) (79, 82) -> 79  
+		//see if x is greater than y
+		if ((x <= y) && (-x >= y)) { // if x is negative AND absolute value of x is > y
+			// if it is, modulo( x+y)
+			 return modulo(x+y, y); // (-2,2)
+		}else if (x <= y && -x < y) { // if x is negative AND absolute value of X is < y
+			// if it isn't , return x
+			return x;	
+		}
+	}
+};
+
+// 12. Write a function that multiplies two numbers without using the * operator or
+// Math methods.
+var multiply = function(x, y) {  //(-2, 4) 
+	if(y === 0 || x === 0){
+		return 0;
+	}
+
+	if((y < 0) && (x < 0)) { //If x and y are negative
+		y = -y
+		x = -x
+	};
+
+	var sum = 0;
+	if( y > 0) {
+		sum += x;
+   } else {
+   	sum -= x;
+   };
+
+	if( y === 1 || y === -1) { //Base case
+		return sum;
+	}
+	if(y > 1){ //If is positive
+		//recurse
+		return sum += multiply(x, --y);
+	};
+	if(y < -1){ //If y is negative  //(2, -2)
+		//recurse
+		return sum += multiply(x, ++y);   //
+	};
+};
+
+
+// 13. Write a function that divides two numbers without using the / operator or
+// Math methods to arrive at an approximate quotient (ignore decimal endings).
+var divide = function(x, y) { 
+	if((x === 0) && (y === 0)){
+		return NaN;
+	}
+	if (x === 0){
+		return 0;
+	}
+	var counter = 0;              
+	if((x > 0) && (y > 0)){
+		if(x >= y){        //  if we have x, y
+			counter++                           
+			counter += divide(x-y, y);   
+		}
+	}
+	if((x < 0) && (y > 0)){ // if we have -x, y   
+		//answer should be negative 
+		if((-x) >= y){ 
+			counter--                          
+			counter += divide(x+y, y);   
+		}
+	}
+	if((x > 0) && (y < 0)){  // if x, -y 
+		//answer should be negative
+		if(x >= y){ // x, y
+			counter--                            
+			counter += divide(x+y, y);   
+		}
+	}
+	if((x < 0) && (y < 0)){    // if -x, -y  
+	//anwer should be positive 
+		if(x <= y){ // x, y 
+			counter++                          
+			counter += divide(x-y, y);   
+		}
+	}
+	return counter;
+};
+
+// 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
+// integers is the greatest integer that divides both x and y with no remainder.
+// gcd(4,36); // 4
+// http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
+// https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
+var gcd = function(x, y) {
+	// we would have two variable arrays
+	var xFactors = []
+	var yFactors = []
+// 1st) all the factors of the first number // 4 -> [1,2,4] 9%9 = 0 
+	var arr1 = '[]'
+//Set the base case to if the last item === x
+	if(){
+
+	}
+	      	// if x is odd 
+	      		// each number x % 2 should === 1
+	      		// return [].push(gcd(x, y)) 
+	     // populated using recursion
+	  // 2nd) all the factors of the 2nd number // 36 -> [1, 2, 3, 4, 6, 12, 36]
+	      // populated using recursion
+
+
+	  //for loop iterate on each element of first variable array
+	     // compare to each number of the second array
+	     // when 1st  num === 2nd num
+
+};
+
+// 15. Write a function that compares each character of two strings and returns true if
+// both are identical.
+// compareStr('house', 'houses') // false
+// compareStr('tomato', 'tomato') // true
+var compareStr = function(str1, str2) {
+};
+
+// 16. Write a function that accepts a string and creates an array where each letter
+// occupies an index of the array.
+var createArray = function(str) {
+};
+
+// 17. Reverse the order of an array
+var reverseArr = function(array) {
+};
+
+// 18. Create a new array with a given value and length.
+// buildList(0,5) // [0,0,0,0,0]
+// buildList(7,3) // [7,7,7]
+var buildList = function(value, length) {
+};
+
+// 19. Implement FizzBuzz. Given integer n, return an array of the string representations of 1 to n.
+// For multiples of three, output 'Fizz' instead of the number.
+// For multiples of five, output 'Buzz' instead of the number.
+// For numbers which are multiples of both three and five, output “FizzBuzz” instead of the number.
+// fizzBuzz(5) // ['1','2','Fizz','4','Buzz']
+var fizzBuzz = function(n) {
+};
+
+// 20. Count the occurence of a value in a list.
+// countOccurrence([2,7,4,4,1,4], 4) // 3
+// countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
+var countOccurrence = function(array, value) {
+};
+
+// 21. Write a recursive version of map.
+// rMap([1,2,3], timesTwo); // [2,4,6]
+var rMap = function(array, callback) {
+};
+
+// 22. Write a function that counts the number of times a key occurs in an object.
+// var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
+// countKeysInObj(obj, 'r') // 1
+// countKeysInObj(obj, 'e') // 2
+var countKeysInObj = function(obj, key) {
+};
+
+// 23. Write a function that counts the number of times a value occurs in an object.
+// var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
+// countValuesInObj(obj, 'r') // 2
+// countValuesInObj(obj, 'e') // 1
+var countValuesInObj = function(obj, value) {
+};
+
+// 24. Find all keys in an object (and nested objects) by a provided name and rename
+// them to a provided new name while preserving the value stored at that key.
+var replaceKeysInObj = function(obj, oldKey, newKey) {
+};
+
+// 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
+// number is the sum of the previous two.
+// Example: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.....
+// fibonacci(5); // [0,1,1,2,3,5]
+// Note: The 0 is not counted.
+var fibonacci = function(n) {
+};
+
+// 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
+// [0,1,1,2,3,5,8,13,21]
+// nthFibo(5); // 5
+// nthFibo(7); // 13
+// nthFibo(3); // 2
+var nthFibo = function(n) {
+};
+
+// 27. Given an array of words, return a new array containing each word capitalized.
+// var words = ['i', 'am', 'learning', 'recursion'];
+// capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
+var capitalizeWords = function(array) {
+};
+
+// 28. Given an array of strings, capitalize the first letter of each index.
+// capitalizeFirst(['car','poop','banana']); // ['Car','Poop','Banana']
+var capitalizeFirst = function(array) {
+};
+
+// 29. Return the sum of all even numbers in an object containing nested objects.
+// var obj1 = {
+//   a: 2,
+//   b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+//   c: {c: {c: 2}, cc: 'ball', ccc: 5},
+//   d: 1,
+//   e: {e: {e: 2}, ee: 'car'}
+// };
+// nestedEvenSum(obj1); // 10
+var nestedEvenSum = function(obj) {
+};
+
+// 30. Flatten an array containing nested arrays.
+// flatten([1,[2],[3,[[4]]],5]); // [1,2,3,4,5]
+var flatten = function(array) {
+};
+
+// 31. Given a string, return an object containing tallies of each letter.
+// letterTally('potato'); // {p:1, o:2, t:2, a:1}
+var letterTally = function(str, obj) {
+};
+
+// 32. Eliminate consecutive duplicates in a list. If the list contains repeated
+// elements they should be replaced with a single copy of the element. The order of the
+// elements should not be changed.
+// compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
+// compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
+var compress = function(list) {
+};
+
+// 33. Augument every element in a list with a new value where each element is an array
+// itself.
+// augmentElements([[],[3],[7]], 5); // [[5],[3,5],[7,5]]
+var augmentElements = function(array, aug) {
+};
+
+// 34. Reduce a series of zeroes to a single 0.
+// minimizeZeroes([2,0,0,0,1,4]) // [2,0,1,4]
+// minimizeZeroes([2,0,0,0,1,0,0,4]) // [2,0,1,0,4]
+var minimizeZeroes = function(array) {
+};
+
+// 35. Alternate the numbers in an array between positive and negative regardless of
+// their original sign. The first number in the index always needs to be positive.
+// alternateSign([2,7,8,3,1,4]) // [2,-7,8,-3,1,-4]
+// alternateSign([-2,-7,8,3,-1,4]) // [2,-7,8,-3,1,-4]
+var alternateSign = function(array) {
+};
+
+// 36. Given a string, return a string with digits converted to their word equivalent.
+// Assume all numbers are single digits (less than 10).
+// numToText("I have 5 dogs and 6 ponies"); // "I have five dogs and six ponies"
+var numToText = function(str) {
+};
+
+
+// *** EXTRA CREDIT ***
+
+// 37. Return the number of times a tag occurs in the DOM.
+var tagCount = function(tag, node) {
+};
+
+// 38. Write a function for binary search.
+// var array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+// binarySearch(array, 5) // 5
+// https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search
+var binarySearch = function(array, target, min, max) {
+};
+
+// 39. Write a merge sort function.
+// mergeSort([34,7,23,32,5,62]) // [5,7,23,32,34,62]
+// https://www.khanacademy.org/computing/computer-science/algorithms/merge-sort/a/divide-and-conquer-algorithms
+var mergeSort = function(array) {
+};
+
+// 40. Deeply clone objects and arrays.
+// var obj1 = {a:1,b:{bb:{bbb:2}},c:3};
+// var obj2 = clone(obj1);
+// console.log(obj2); // {a:1,b:{bb:{bbb:2}},c:3}
+// obj1 === obj2 // false
+var clone = function(input) {
+};
